86dc852491e479ae931947c68ece6ecf73480e13
linghan <ryou0634@gmail.com>
2021-11-04 20:07:28 +0900

    debug

--- hanabi/game_field/hanabi_field.py
+++ hanabi/game_field/hanabi_field.py
@@ -1,15 +1,14 @@
-from hanabi.objects import Card, Color, FailureTokensOnField
-from hanabi.game_field import Colorfield
+from hanabi.objects import Card
+from hanabi.game_field.color_field import ColorField
 from typing import List
 
-class Hanabifield:
-    def __init__(self, colorfields: List[Colorfield]):
-        self.colorfields = {field.color: field for field in colorfields}
 
-    def is_able_to_add(self, card):
-        return self.colorfields[card.color].is_able_to_add(card)
+class HanabiField:
+    def __init__(self, color_fields: List[ColorField]):
+        self.color_fields = {field.color: field for field in color_fields}
 
+    def is_able_to_add(self, card: Card) -> bool:
+        return self.color_fields[card.color].is_able_to_add(card)
 
-    def add_card(self, card):
-        self.colorfields[card.color].add_card(card)
-            
+    def add_card(self, card: Card):
+        self.color_fields[card.color].add_card(card)
