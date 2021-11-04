126582439aeb057a84bf8b0ce4b74461ed4af230
linghan <ryou0634@gmail.com>
2021-11-04 19:26:15 +0900

    refactoring

--- hanabi/game_field/color_field.py
+++ hanabi/game_field/color_field.py
@@ -1,25 +1,21 @@
 from hanabi.objects import Card, Color
 
-class Colorfield:
-    def __init__(self, color:Color):
 
+class ColorField:
+    def __init__(self, color: Color):
         self.color = color
         self.cards = []
 
-    def is_able_to_add(self, card: Card):
+    def is_able_to_add(self, card: Card) -> bool:
         if self.color != card.color:
             return False
 
-        if len(self.cards) + 1 != card.number:
+        top_card_number = self.cards[-1].number
+        if top_card_number.value + 1 != card.number.value:
             return False
         return True
 
     def add_card(self, card: Card):
         if not self.is_able_to_add():
-            raise RuntimeError(
-                "card type is mistaken"
-                            )
+            raise RuntimeError("card type is mistaken")
         self.cards.append(card)
-
-
-
